8475088d0925330ba7fb8fba25be2813c07f01e8
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-05-25 17:58:31 -0300

    ex010

--- ex007/ex008.js
+++ ex007/ex008.js
@@ -1,1 +1,6 @@
-CSSCounterStyleRule.log('O console funcinou!')
+var vel = 61
+console.log(`A velocidade do carro é ${vel} km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança`)
