639dc0b7e2957fa4ea7057c05b173288c4288a2f
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-05-25 18:33:12 -0300

    up

---
+++ ex008/ex013.js
@@ -0,0 +1,29 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+console.log(diaSem)
+
+switch(diaSem) {
+    case 1:
+        console.log('Domingo')
+        break
+    case 2:
+        console.log('Segunda')
+        break
+    case 3:
+        console.log('Terça')
+        break
+    case 4:
+        console.log('Quarta')
+        break
+    case 5:
+        console.log('Quinta')
+        break
+    case 6:
+        console.log('Sexta')
+        break
+    case 7:
+        console.log('Sábado')
+        break
+    default: 
+        console.log('[ERRO] Dia inválido')
+}
