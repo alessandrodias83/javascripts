bf87a7c6eca32c2bcd6c3a1fbc4e4dc996667508
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-05-26 16:11:54 -0300

    up

---
+++ ex009/ex013.js
@@ -0,0 +1,17 @@
+var c = 1 // teste de repetição com repetição no final
+do {
+    console.log(`Passo ${c}`)
+    c++
+}
+while(c <= 6) 
+
+
+
+
+/*var c = 1
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+// teste lógico de repetição no inicio 
